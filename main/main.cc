e417c7e487206f5663750f0bea2e2207e2569d9e
Pietrek xD <pietrek.j@gmail.com>
2023-08-19 23:08:57 +0200

    init project

---
+++ main/main.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+
+extern "C" void app_main(void) {
+    int i = 0;
+    while (1) {
+        printf("[%d] Hello world!\n", i);
+        i++;
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
